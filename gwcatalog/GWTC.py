a5a02e1a399dc4b0101b51b164f2d86c7a5d57f7
JPUnD <jose@jpferreira.me>
2021-11-15 15:21:44 +0000

    GWTC now only returns data from catalog itself

--- gwcatalog/GWTC.py
+++ gwcatalog/GWTC.py
@@ -3,6 +3,7 @@
 
 
 # imports
+from scipy.misc import derivative
 from random import gauss
 import pandas
 
@@ -12,33 +13,39 @@ from .cosmology import H, dL
 
 
 # generate GWTC events
-def generate(ideal=False, multiplier=1):
+def generate():
     # get raw data from the GWTC file
     with open(__file__.replace("GWTC.py", "") + "data/GWTC.csv", "r") as file:
         columns = pandas.read_csv(file, comment="#")
         distances_mid = columns["luminosity_distance"]
         distances_lower = columns["luminosity_distance_lower"]
         distances_upper = columns["luminosity_distance_upper"]
+        redshifts_mid = columns["redshift"]
+        redshifts_lower = columns["redshift_lower"]
+        redshifts_upper = columns["redshift_upper"]
 
     # symmetrize luminosity distance errors and convert Mpc to Gpc
     distances = []
-    errors = []
+    disterrors = []
     for i in range(0, len(distances_mid)):
         distance = ( distances_mid[i] + (distances_upper[i] + distances_lower[i])/2 ) / 1000
-        error = ( (distances_upper[i] - distances_lower[i])/2 ) / 1000
+        disterror = ( (distances_upper[i] - distances_lower[i])/2 ) / 1000
         distances.append(distance)
-        errors.append(multiplier*error)
+        disterrors.append(disterror)
 
-    # get redshifts from the events luminosity distance
-    # assumes that highest redshift is 1 and uses 2000 steps in between z = 0 and z = 1
-    redshifts = dL_to_redshift(distances, 0, 1, 2000)
+    # symmetrize the redshift error
+    redshifts = []
+    rederrors = []
+    for i in range(0, len(redshifts_mid)):
+        redshift = ( redshifts_mid[i] + (redshifts_upper[i] + redshifts_lower[i])/2 )
+        rederror = ( (redshifts_upper[i] - redshifts_lower[i])/2 )
+        redshifts.append(redshift)
+        rederrors.append(rederror)
 
-    # generate new luminosity distances based on the real luminosity distances, with a gaussian error
-    if not ideal:
-        for i in range(0, len(distances)):
-            rnd = -1
-            while rnd < 0:
-                rnd = gauss(distances[i], errors[i])
-            distances[i] = round(rnd, 3)
+    # propagate the redshift error to the luminosity distance
+    errors = []
+    for i in range(0, len(redshifts_mid)):
+        propagatedredshifterror = derivative(dL, redshifts[i], dx=1e-6, args=(H,)) * rederrors[i]
+        errors.append((disterrors[i]**2 + propagatedredshifterror**2)**0.5)
 
     return redshifts, distances, errors
