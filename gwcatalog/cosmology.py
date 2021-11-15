114a3de5ec3b5c16601b7cada385153ee675d180
JPUnD <jose@jpferreira.me>
2021-11-15 14:29:46 +0000

    minor tidy

--- gwcatalog/cosmology.py
+++ gwcatalog/cosmology.py
@@ -9,15 +9,15 @@ from scipy.integrate import quad
 # Hubble function
 def H(z):
     # option 1: using c/H0 = (2.9979 Gpc)/h and providing the value for h
-    #h = 0.7
-    #H0 = 299792458*h/(2.9979*3.085678*10**25)
+    h = 0.7
+    H0 = 299792458*h/(2.9979*3.085678*10**25)
 
     # option 2: providing the value for H0 in units of [(km/s)/Mpc]
     #H0 = 70
     #H0 = H0*3.240779289*10**(-20)
 
     # option 3: providing the value for H0 directly in [s⁻¹]
-    H0 = 2.2685641027252412e-18  # Hubble constant [s⁻¹]
+    #H0 = 2.2685641027252412e-18
 
     # value for Ωm
     Ωm = 0.284
