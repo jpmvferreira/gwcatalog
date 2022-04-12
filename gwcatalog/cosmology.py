11c9c5fdb381cac8faa162363dd765add7ed5bd7
JPUnD <jose@jpferreira.me>
2022-04-12 17:20:11 +0100

    parameters are now Planck2018 best fit

--- gwcatalog/cosmology.py
+++ gwcatalog/cosmology.py
@@ -1,5 +1,5 @@
 ## cosmology.py
-# default cosmological model: ΛCDM with Ωm = 0.284 and h = 0.7
+# default cosmological model: ΛCDM with Ωm = 0.315 and H0 = 67.4
 
 
 # imports
@@ -8,19 +8,12 @@ from scipy.integrate import quad
 
 # Hubble function
 def H(z):
-    # option 1: using c/H0 = (2.9979 Gpc)/h and providing the value for h
-    h = 0.7
-    H0 = 299792458*h/(2.9979*3.085678*10**25)
-
-    # option 2: providing the value for H0 in units of [(km/s)/Mpc]
-    #H0 = 70
-    #H0 = H0*3.240779289*10**(-20)
-
-    # option 3: providing the value for H0 directly in [s⁻¹]
-    #H0 = 2.2685641027252412e-18
+    # provide the value for H0 in units of (km/s)/Mpc and convert it so s⁻¹
+    H0 = 67.4
+    H0 = H0*3.240779289*10**(-20)
 
     # value for Ωm
-    Ωm = 0.284
+    Ωm = 0.315
 
     return H0*(Ωm*(1+z)**3 + 1-Ωm)**0.5
 
