cfba46025292864940f9ad41ca90a336f1d535d5
JPUnD <jose@jpferreira.me>
2022-08-03 14:14:27 +0100

    default cosmology is back to Ωm = 0.284 and h = 0.7

--- gwcatalog/cosmology.py
+++ gwcatalog/cosmology.py
@@ -1,5 +1,5 @@
 ## cosmology.py
-# default cosmological model: ΛCDM with Ωm = 0.315 and H0 = 67.4
+# default cosmological model: ΛCDM with Ωm = 0.284 and h = 0.7
 
 
 # imports
@@ -8,12 +8,12 @@ from scipy.integrate import quad
 
 # Hubble function
 def H(z):
-    # provide the value for H0 in units of (km/s)/Mpc and convert it so s⁻¹
-    H0 = 67.4
-    H0 = H0*3.240779289*10**(-20)
+    # define Hubble's constant using c/H0 = (2.9979 Gpc)/h and providing the value for h
+    h = 0.7
+    H0 = 299792458*h/(2.9979*3.085678*10**25)
 
     # value for Ωm
-    Ωm = 0.315
+    Ωm = 0.284
 
     return H0*(Ωm*(1+z)**3 + 1-Ωm)**0.5
 
